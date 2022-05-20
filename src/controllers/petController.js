3bc5a8d417549e9835aa77a459d007a6c1d0d672
medusa <medusa_piedra@hotmail.com>
2022-05-20 11:54:33 -0400

    siipi

--- src/controllers/petController.js
+++ src/controllers/petController.js
@@ -1,17 +1,21 @@
 const Pet = require('../models/petModel');
+const bcrypt = require('bcrypt');
 
 exports.post = async (req, res) => {
     const {name, certificate, age, gender, owner, email, password} = req.body;
-    const pet_found = await Pet.findOne({certificate}); 
+            
+    const pet_found = await Pet.findOne({certificate});
     if(pet_found){
         res.status(410).json({
             error: 401,
             message: 'Esta mascota ya fue registrada'
         });
     }else{
-        const newPet = new Pet({name, certificate, age, gender, owner, email, password});
+        const salt = await bcrypt.genSalt(10);
+        const newpassword = await bcrypt.hash(password, salt);
+        const newPet = new Pet({name, certificate, age, gender, owner, email, password: newpassword});
         await newPet.save();
-        res.json({message: "mascota grabada correctamente"})
+        res.json({message: "mascota registrada correctamente"})
     }
 },
 
@@ -40,5 +44,18 @@ exports.put = (req, res) => {
 },
 
 exports.delete = (req, res) => {
-    res.json({message: 'llego datos delete', data: req.params});
+    const {id} = req.params;
+    Pet.findByIdAndDelete(id, (err, pet) => {
+        if(err){
+            res.status(500).json({
+                error: 500,
+                message: 'Error al eliminar mascota'
+            });
+        }else{
+            res.json({
+                error: null,
+                message: 'Mascota eliminada correctamente'
+            });
+        }
+    });
 }
