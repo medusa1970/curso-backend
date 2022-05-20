009e9ee83d825f6adeba1292aeb0b0f9a530eaed
medusa <medusa_piedra@hotmail.com>
2022-05-20 12:21:23 -0400

    listoo

--- src/controllers/petController.js
+++ src/controllers/petController.js
@@ -1,5 +1,6 @@
 const Pet = require('../models/petModel');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 exports.post = async (req, res) => {
     const {name, certificate, age, gender, owner, email, password} = req.body;
@@ -35,15 +36,60 @@ exports.get = async (req, res) => {
     }
 },
 
-exports.login = (req, res) => {
-    res.json({message: 'llego datos login', data: req.body});
-},
+exports.login = async(req, res) => {
+    const {certificate, password} = req.body;  //req.body es un objeto
+    const pet = await Pet.findOne({certificate}); //busca en la base de datos el usuario con el certificado
+    if(!pet){ //si no encuentra el usuario
+        res.status(401).json({
+            error: 401,
+            message: 'No existe una mascota con este numero de certificado'
+        });
+    }else{
+        const validPassword = await bcrypt.compare(password, pet.password);
+        if(!validPassword){
+            res.status(401).json({
+                error: 401,
+                message: 'Contraseña incorrecta'
+            });
+        }else{
+            const token = jwt.sign({ _id: pet._id}, process.env.SECRET_TOKEN, { expiresIn: '5m'});
+            const refreshtoken = jwt.sign({ _id: pet._id}, process.env.SECRET_TOKEN, { expiresIn: '10m'});
+            res.json({
+                error: null,
+                message: 'Login exitoso',
+                token,
+                refreshtoken
+            });
+        }
+            res.json({
+                error: null,
+                message: 'Login correcto',
+                pet
+            });
+        }
+}
+
 
 exports.put = (req, res) => {
-    res.json({message: 'llego datos update', data: req.params});
-},
+    const {id} = req.params;
+    const {owner, email} = req.body;
+    Pet.findByIdAndUpdate(id, {owner, email}, {new: true}, (err, pet) => {
+        if(err){
+            res.status(500).json({
+                error: 500,
+                message: 'Error al actualizar mascota'
+            });
+        }else{
+            res.json({
+                error: null,
+                message: 'Mascota actualizada correctamente',
+                pet
+            });
+        }
+    });
+}
 
-exports.delete = (req, res) => {
+exports.delete = async(req, res) => {
     const {id} = req.params;
     Pet.findByIdAndDelete(id, (err, pet) => {
         if(err){
